c5073b3d3ddb6b048c0acbce038cfca2129d15dd
Shivam Patil <patilshivam156@gmail.com>
2024-02-22 01:47:19 +0530

    Create a model for user and a post route for register

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const user = require("./model/User");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
+app.use(express.json());
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+
 mongoose.connect(process.env.MONGODB_URL);
 
-app.get("/test", (req, res) => {
-    res.json("This is a test response!!!");
-})
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const userDetails = await user.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt)
+        })
+        res.json(userDetails);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+
+});
 
 const PORT = process.env.PORT;
 
