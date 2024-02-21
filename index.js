3cf5000158e3cdc81ef9beef702e4c81f5f7fb8a
Shivam Patil <patilshivam156@gmail.com>
2024-02-22 01:52:49 +0530

    Add cors

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const user = require("./model/User");
 const bcrypt = require("bcrypt");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 const bcryptSalt = bcrypt.genSaltSync(10);
 
